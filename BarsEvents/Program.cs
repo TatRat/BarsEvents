bdfe0bc998b641fe2d1e270ddba7cdbd775ea841
Womp_Rat <im.big.rat@gmail.com>
2022-04-04 00:53:04 +0300

    Initial commit

---
+++ BarsEvents/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BarsEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyboardReader reader = new KeyboardReader();
+            reader.OnKeyPresed += Symbol;
+            reader.Run();
+        }
+        public static void Symbol(object sender, char symbol)
+        {
+            Console.WriteLine($"\n{symbol}");
+        }
+    }
+
+    public class KeyboardReader
+    {
+        public event EventHandler<char> OnKeyPresed;
+        public void Run()
+        {
+            char pressedKeyChar = Console.ReadKey().KeyChar;
+            while (pressedKeyChar != 'c')
+            {
+                pressedKeyChar = Console.ReadKey().KeyChar;
+                OnKeyPresed?.Invoke(this,pressedKeyChar);
+            }
+        }
+    }
+}
